83d00cd9b753137b461b066e98dea7aeb77036b4
Rishav Ghosh <rishavghosh2007@gmail.com>
2026-02-18 11:59:58 +0530

    ses 8

---
+++ RPRG_8.R
@@ -0,0 +1,18 @@
+ggplot(mtcars, aes(cyl, mpg)) + geom_col()
+
+mtcars|>
+  filter(gear==3)|>
+  select(mpg, disp)
+  
+
+#use lubridate for the dates
+
+stocks25=read_delim("stocks25.txt", )
+
+stocks25$co_stkdate <- dmy(stocks25$co_stkdate)
+filter(stocks==s
+       
+       
+       #monthly returns, yearly returns, 2025 returns, 
+       c=0+sqrt(-1)
+       class(c)
